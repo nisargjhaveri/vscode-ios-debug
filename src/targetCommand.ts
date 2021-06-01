0b6bd4fe7811d7e1328a8082da238e86223a2829
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2021-06-01 15:01:27 +0530

    Make udid optional in target commands, defaults to target picker

--- src/targetCommand.ts
+++ src/targetCommand.ts
@@ -1,26 +1,42 @@
+import * as vscode from 'vscode';
 import { boot, install, launch } from './simulators';
 
-export async function simulatorInatall({udid, path}: {udid: string, path: string})
+async function resolveArgs(args: any)
 {
+	if (!args.udid)
+	{
+		args.udid = await vscode.commands.executeCommand('ios-device.targetUDID');
+	}
+
+	return args;
+}
+
+export async function simulatorInatall(args: {udid: string, path: string})
+{
+	let {udid, path} = await resolveArgs(args);
+
 	return Promise.resolve()
 		.then(() => boot(udid))
 		.then(() => install(udid, path));
 }
 
-export async function simulatorLaunch({udid, bundleId}: {udid: string, bundleId: string})
+export async function simulatorLaunch(args: {udid: string, bundleId: string})
 {
+	let {udid, bundleId} = await resolveArgs(args);
+
 	return Promise.resolve()
 		.then(() => boot(udid))
 		.then(() => launch(udid, bundleId, true))
 		.then((pid) => pid.toString());
 }
 
-export async function simulatorInstallLaunch({udid, path, bundleId}: {udid: string, path: string, bundleId: string})
+export async function simulatorInstallLaunch(args: {udid: string, path: string, bundleId: string})
 {
+	let {udid, path, bundleId} = await resolveArgs(args);
+
 	return Promise.resolve()
 		.then(() => boot(udid))
 		.then(() => install(udid, path))
 		.then(() => launch(udid, bundleId, true))
 		.then((pid) => pid.toString());
 }
-
